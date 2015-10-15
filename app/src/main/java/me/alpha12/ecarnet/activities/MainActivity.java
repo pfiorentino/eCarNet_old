b83299722c710ef6b58d3d013f49d25ea3d385b9
Paul FIORENTINO <fiorentino.paulalain@gmail.com>
2015-10-15 15:20:28 +0200

    Empty message (alias PleinG de modifes)

--- app/src/main/java/me/alpha12/ecarnet/activities/MainActivity.java
+++ app/src/main/java/me/alpha12/ecarnet/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package me.alpha12.ecarnet.activities;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,13 +15,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import me.alpha12.ecarnet.R;
 import me.alpha12.ecarnet.classes.Car;
+import me.alpha12.ecarnet.classes.Model;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    public ArrayList<Car> cars = new ArrayList<>();
+    public Car currentCar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +38,15 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Car car = new Car();
-        car.name = "Renault Clio 2.2";
-        car.desc = "71 AFB 34 - 65ch 1.5l DCI";
+        Model model1 = new Model("Renault", "Clio 2.2", "1.5l DCI 65ch");
+        Model model2 = new Model("Peugeot", "206+", "1.4l 70ch");
+        Model model3 = new Model("Citroën", "Saxo", "1.0l 50ch");
+
+        cars.add(new Car("71 AFB 34", model1));
+        cars.add(new Car("CT 091 DQ", model2));
+        cars.add(new Car("XX 180 TG", model3));
+
+        changeCar(cars.get(0));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -45,12 +62,6 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
-        TextView drawerTitle = (TextView) findViewById(R.id.car_name);
-        drawerTitle.setText(car.name);
-
-        TextView drawerDesc = (TextView) findViewById(R.id.car_desc);
-        drawerDesc.setText(car.desc);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -94,22 +105,110 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_gas) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_repair) {
 
         }
 
+        closeDrawer();
+
+        return true;
+    }
+
+    public void closeDrawer() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
+    }
+
+    public void changeCar(Car newCar) {
+        LinearLayout header = (LinearLayout) findViewById(R.id.drawer_header);
+        ImageView brandImageView = (ImageView) findViewById(R.id.brand_image_view);
+
+        currentCar = newCar;
+
+        TextView drawerTitle = (TextView) findViewById(R.id.car_name);
+        drawerTitle.setText(currentCar.model.brand+" "+currentCar.model.model);
+
+        TextView drawerDesc = (TextView) findViewById(R.id.car_desc);
+        drawerDesc.setText(currentCar.plateNum+" - "+currentCar.model.engine);
+
+        switch (currentCar.model.brand){
+            case "Renault":
+                header.setBackgroundResource(R.drawable.background_renault);
+                brandImageView.setImageResource(R.mipmap.ic_renault);
+                break;
+            case "Peugeot":
+                header.setBackgroundResource(R.drawable.background_peugeot);
+                brandImageView.setImageResource(R.mipmap.ic_peugeot);
+                break;
+            case "Citroën":
+                header.setBackgroundResource(R.drawable.background_citroen);
+                brandImageView.setImageResource(R.mipmap.ic_citroen);
+                break;
+        }
+
+        LinearLayout carsLayout = (LinearLayout) findViewById(R.id.cars_icon_layout);
+        carsLayout.removeViews(2, carsLayout.getChildCount() - 2);
+
+        for (Car car : cars) {
+            if (currentCar != null && !car.plateNum.equals(currentCar.plateNum)) {
+                ImageView carImage = new ImageView(getBaseContext());
+                switch (car.model.brand) {
+                    case "Renault":
+                        carImage.setImageResource(R.mipmap.ic_renault);
+                        break;
+                    case "Peugeot":
+                        carImage.setImageResource(R.mipmap.ic_peugeot);
+                        break;
+                    case "Citroën":
+                        carImage.setImageResource(R.mipmap.ic_citroen);
+                        break;
+                }
+
+                int size = (int) getResources().getDimension(R.dimen.other_car_size);
+                int spacing = (int) getResources().getDimension(R.dimen.other_car_spacing);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(size, size);
+                layoutParams.setMargins(spacing, 0, 0, 0);
+                carImage.setLayoutParams(layoutParams);
+                carImage.setClickable(true);
+                carImage.setTag(car);
+
+                carImage.setOnTouchListener(new View.OnTouchListener() {
+
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN: {
+                                ImageView view = (ImageView) v;
+                                //overlay is black with transparency of 0x77 (119)
+                                view.getDrawable().setColorFilter(getResources().getColor(R.color.colorAccentAlpha), PorterDuff.Mode.SRC_ATOP);
+                                view.invalidate();
+                                break;
+                            }
+                            case MotionEvent.ACTION_UP: {
+                                Car associatedCar = (Car) v.getTag();
+                                changeCar(associatedCar);
+                            }
+                            case MotionEvent.ACTION_CANCEL: {
+                                ImageView view = (ImageView) v;
+                                //clear the overlay
+                                view.getDrawable().clearColorFilter();
+                                view.invalidate();
+                                break;
+                            }
+                        }
+
+                        return true;
+                    }
+                });
+
+                carsLayout.addView(carImage);
+            }
+        }
+
+        closeDrawer();
     }
 }
--- app/src/main/java/me/alpha12/ecarnet/classes/Car.java
+++ app/src/main/java/me/alpha12/ecarnet/classes/Car.java
@@ -4,6 +4,11 @@ package me.alpha12.ecarnet.classes;
  * Created by paul on 02/10/2015.
  */
 public class Car {
-    public String name;
-    public String desc;
+    public String plateNum;
+    public Model model;
+
+    public Car(String plateNum, Model model) {
+        this.plateNum = plateNum;
+        this.model = model;
+    }
 }
